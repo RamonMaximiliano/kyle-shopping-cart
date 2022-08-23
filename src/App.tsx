3d8a0af0a97989405b5926d7a376a06579f7dcab
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-08-22 21:55:06 -0300

    Starting

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+...
     </div>
   );
 }
 
 export default App;
+
+
+
+/* 
+
+
+https://www.youtube.com/watch?v=lATafp15HWA&ab_channel=WebDevSimplified
+
+
+
+
+*/
