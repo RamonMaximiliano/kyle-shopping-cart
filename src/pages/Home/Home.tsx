f73828c2ca90b59e7a944a7ecc875940cf766d9b
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-09-06 00:07:44 -0300

    images

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,56 @@
+
 export function Home() {
     return (
-        <div>Home test</div>
+        <div>
+            <h3>Home test</h3>
+
+        <div>
+            <img src='./images/fogão.JPG'></img>    
+        </div>    
+        <div>
+            <img src='./images/liquidificador.JPG'></img>    
+        </div>    
+        <div>
+            <img src='./images/micro.JPG'></img>    
+        </div>    
+        <div>
+            <img src='./images/torradeira.JPG'></img>    
+        </div>    
+        <div>
+            <img src='./images/geladeira.JPG'></img>    
+        </div>    
+        <div>
+            <img src='./images/mesa.JPG'></img>    
+        </div>    
+        
+        <div>
+            <img src='./images/sofa.JPG'></img>    
+        </div>    
+        
+        <div>
+            <img src='./images/lavadoura.JPG'></img>    
+        </div>    
+        
+        <div>
+            <img src='./images/tv.JPG'></img>    
+        </div>    
+        
+        
+        
+        
+        
+        </div>
     )
 }
+
+
+/* 
+
+https://www.youtube.com/watch?v=XHfOyhkdlgc&ab_channel=OpenSource
+
+
+https://www.youtube.com/watch?v=0wvrlOyGlq0&ab_channel=CodingSnow 
+
+https://www.youtube.com/watch?v=KcdBOoK3Pfw&ab_channel=DevEd
+
+*/
