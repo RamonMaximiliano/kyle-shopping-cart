520c5ad144483552251a7cd5f0b55b53e12c1d23
Ramon <ramon_max1986@hotmail.com>
2022-09-06 13:43:02 -0300

    imageslider

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,45 +1,70 @@
+import './styles.css'
 
 export function Home() {
     return (
-        <div>
-            <h3>Home test</h3>
+        <>
+            <div>
+                <h3>Home test</h3>
+            </div>
+            <div className="slide-area">
+                <div className="slide-images">
+                    <div>
+                        <div className="torradeira">hgello</div>
+                    </div>
+                    <div>
+                        <img src='./images/torradeira.JPG'></img>
+                    </div>
+                    <div>
+                        <img src='./images/micro.JPG'></img>
+                    </div>
+                    <div>
+                        <img src='./images/fogão.JPG'></img>
+                    </div>
+                     <div>
+                        <img src='./images/liquidificador.JPG'></img>
+                    </div>
+                    <div>
+                        <img src='./images/geladeira.JPG'></img>
+                    </div>
+                    <div>
+                        <img src='./images/mesa.JPG'></img>
+                    </div>
 
-        <div>
-            <img src='./images/fogão.JPG'></img>    
-        </div>    
-        <div>
-            <img src='./images/liquidificador.JPG'></img>    
-        </div>    
-        <div>
-            <img src='./images/micro.JPG'></img>    
-        </div>    
-        <div>
-            <img src='./images/torradeira.JPG'></img>    
-        </div>    
-        <div>
-            <img src='./images/geladeira.JPG'></img>    
-        </div>    
-        <div>
-            <img src='./images/mesa.JPG'></img>    
-        </div>    
-        
-        <div>
-            <img src='./images/sofa.JPG'></img>    
-        </div>    
-        
-        <div>
-            <img src='./images/lavadoura.JPG'></img>    
-        </div>    
-        
-        <div>
-            <img src='./images/tv.JPG'></img>    
-        </div>    
-        
-        
-        
-        
-        
-        </div>
+                    <div>
+                        <img src='./images/sofa.JPG'></img>
+                    </div>
+
+                    <div>
+                        <img src='./images/lavadoura.JPG'></img>
+                    </div>
+
+                    <div>
+                        <img src='./images/tv.JPG'></img>
+                    </div>
+                </div>
+
+                <div className="radios">
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                    <input type="radio"></input>
+                </div>
+
+                <div className="buttons">
+                    <div>
+                        <button>Forward</button>
+                    </div>
+                    <div>
+                        <button>Back</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
